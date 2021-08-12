049d88f2397c06623fa72a99e1704e2e19be2542
hellokunal753 <hellokunal753@gmail.com>
2021-08-12 10:15:53 +0530

    Ancient bringup

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,12 +1,12 @@
 #
-# Copyright (C) 2020 Havoc-OS
+# Copyright (C) 2020 ancient-OS
 #
 # SPDX-License-Identifier: Apache-2.0
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/havoc_X01BD.mk
+    $(LOCAL_DIR)/ancient_X01BD.mk
 
 COMMON_LUNCH_CHOICES := \
-    havoc_X01BD-user \
-    havoc_X01BD-userdebug
+    ancient_X01BD-user \
+    ancient_X01BD-userdebug
